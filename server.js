b077c7bfa57efb806bb244e1ff50f538c04d0bd6
Antaljr10 <96958607+Antaljr10@users.noreply.github.com>
2025-04-03 11:49:03 +0200

    reg/login routes

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const cors = require("cors");
 
 const sequelize = require("./config/database");
 const mangaRoutes = require("./routes/mangaRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use("/manga",mangaRoutes)
+app.use("/auth",authRoutes)
 app.use(express.json());
 app.use(cors());
 app.use("/uploads", express.static("uploads")); // Serve uploaded images
